c41435663f0b34be74eb0429d9b40489eedf2c7b
TatuMakinen <tatu.r.makinen@gmail.com>
2020-03-28 16:43:06 +0200

    Updates

---
+++ precache-manifest.86834742738dad6fd5597a62556e9c3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e5979bdb57877214f7f522463f5ee7",
+    "url": "/metacritic/index.html"
+  },
+  {
+    "revision": "5b856627ec79c788c25a",
+    "url": "/metacritic/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9f823049f3cbccc9c6a",
+    "url": "/metacritic/static/js/2.0b6e13a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/metacritic/static/js/2.0b6e13a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b856627ec79c788c25a",
+    "url": "/metacritic/static/js/main.552b2114.chunk.js"
+  },
+  {
+    "revision": "d3ebe7a6553005680c8d",
+    "url": "/metacritic/static/js/runtime-main.744481b8.js"
+  }
+]);
